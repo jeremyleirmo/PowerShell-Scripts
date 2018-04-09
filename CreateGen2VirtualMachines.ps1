9694255266b6c9ced34d78ac9f1aace3a3dcc424
Jeremy Leirmo <jeremyleirmo@yahoo.com>
2018-04-08 22:38:42 -0500

    initial Commit for Home

---
+++ CreateGen2VirtualMachines.ps1
@@ -0,0 +1,53 @@
+$VMLocation = "D:\Virtual Machines"
+$VMISO = "F:\Software\Windows Server\2019\Windows_InsiderPreview_Server_2019.iso"
+$VMNetwork = "Default Switch"
+
+# Create DC01
+$VMName = "WS2019-Server01"
+$VMMemory = 1024MB
+$VMDiskSize = 40GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+Start-VM -Name $VMName
+VMConnect localhost $VMName
+
+# Create CM01
+$VMName = "WS2016-CM01"
+$VMMemory = 16384MB
+$VMDiskSize = 300GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose 
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMProcessor -VMName $VMName -Count 2
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+
+# Create CM02
+$VMName = "WS2016-CM02"
+$VMMemory = 16384MB
+$VMDiskSize = 300GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose 
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMProcessor -VMName $VMName -Count 2
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+
+# Create MDT01
+$VMName = "WS2016-MDT01"
+$VMMemory = 4096MB
+$VMDiskSize = 300GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
+
+
+# Create WSUS01
+$VMName = "WS2016-WSUS01"
+$VMMemory = 4096MB
+$VMDiskSize = 300GB
+New-VM -Name $VMName -Generation 2 -BootDevice CD -MemoryStartupBytes $VMMemory -SwitchName $VMNetwork -Path $VMLocation -NoVHD -Verbose
+New-VHD -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -SizeBytes $VMDiskSize -Verbose
+Add-VMHardDiskDrive -VMName $VMName -Path "$VMLocation\$VMName\Virtual Hard Disks\$VMName-Disk1.vhdx" -Verbose
+Set-VMDvdDrive -VMName $VMName -Path $VMISO -Verbose
